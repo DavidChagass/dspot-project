6678979c9019277a4d1f421a53040041c7702d9a
Caik Lima <124724369+Caik0@users.noreply.github.com>
2024-09-23 06:18:51 -0300

    Começando a alterar os layouts por meio do tailwind css, layout login

--- resources/views/layouts/auth-layout.blade.php
+++ resources/views/layouts/auth-layout.blade.php
@@ -8,10 +8,11 @@
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     @livewireStyles
 </head>
-<body class="font-sans antialiased">
-    <div class="min-h-screen bg-gray-100">
+<body class="w-fit h-fit flex justify-center font-sans">
+    <div class="flex justify-center min-h-screen bg-gray-100">
         <main>
-            {{ $slot }}
+        <!--Adicionar o tipo de login ao layout em um h1 ou h2-->
+            {{ $slot }} <!--Pegar o layout automático para poder padronizar o formulário-->
         </main>
     </div>
     @livewireScripts
