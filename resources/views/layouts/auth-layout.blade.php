7d9ac7154dae3cba26cfc917baf2490f537527c3
Caik Lima <124724369+Caik0@users.noreply.github.com>
2024-09-24 23:09:35 -0300

    login layout

--- resources/views/layouts/auth-layout.blade.php
+++ resources/views/layouts/auth-layout.blade.php
@@ -11,8 +11,7 @@
 <body class="w-fit h-fit flex justify-center font-sans">
     <div class="flex justify-center min-h-screen bg-gray-100">
         <main>
-        <!--Adicionar o tipo de login ao layout em um h1 ou h2-->
-            {{ $slot }} <!--Pegar o layout automático para poder padronizar o formulário-->
+            {{ $slot }}
         </main>
     </div>
     @livewireScripts
