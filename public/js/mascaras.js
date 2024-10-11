da00dc9cc1fd84d2da2c9e9142d3484121933320
Caik Lima <124724369+Caik0@users.noreply.github.com>
2024-10-11 06:33:23 -0300

    mascara cnpj

--- public/js/mascaras.js
+++ public/js/mascaras.js
@@ -74,3 +74,23 @@ function mascaraMutuario(o,f){
       
    return v
   }
+  
+  // Mascara do CNPJ
+  function Cnpj(v){
+  
+    //Remove tudo o que não é dígito
+    v=v.replace(/\D/g,"")
+        //Coloca ponto entre o segundo e o terceiro dígitos
+        v=v.replace(/^(\d{2})(\d)/,"$1.$2")
+  
+        //Coloca ponto entre o quinto e o sexto dígitos
+        v=v.replace(/^(\d{2})\.(\d{3})(\d)/,"$1.$2.$3")
+  
+        //Coloca uma barra entre o oitavo e o nono dígitos
+        v=v.replace(/\.(\d{3})(\d)/,".$1/$2")
+  
+        //Coloca um hífen depois do bloco de quatro dígitos
+        v=v.replace(/(\d{4})(\d)/,"$1-$2")
+    
+    return v
+  }
