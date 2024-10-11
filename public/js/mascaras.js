70af3f862aa2a8b37dbfa5c5a59f69daf72e04e3
Caik Lima <124724369+Caik0@users.noreply.github.com>
2024-10-10 22:49:00 -0300

    Mascara de domínio

---
+++ public/js/mascaras.js
@@ -0,0 +1,38 @@
+// Mascara de Domínio
+document.getElementById("dominio").setAttribute("onkeypress", "mascaraMutuario(this,dom),limitarInputDom(this)");
+document.getElementById("dominio").setAttribute("onblur", "");
+
+
+function mascaraMutuario(o,f){
+    v_obj=o
+    v_fun=f
+    setTimeout('execmascara()',1)
+}
+
+function execmascara(){
+    v_obj.value=v_fun(v_obj.value)
+}
+
+function dom(v){
+
+    //Remove tudo o que não é dígito
+    v=v.replace(/\D/g,"")
+  
+    //Coloca um ponto entre o terceiro e o quarto dígitos
+    v=v.replace(/(\d{5})(\d)/,"$1-$2*")
+
+      
+   return v
+}
+
+function limitarInputDom(obj) {
+    obj.value = obj.value.substring(0,9);
+}
+
+function soNums(e)
+{
+	if (document.all){var evt=event.keyCode;}
+	else{var evt = e.charCode;}
+	if (evt <20 || (evt >47 && evt<58)){return true;}
+	return false;
+}
--- resources/views/layouts/auth-layout.blade.php
+++ resources/views/layouts/auth-layout.blade.php
@@ -8,12 +8,13 @@
     <title>Página de Login</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <script defer src="{{ asset('js/mascaras.js') }}"></script>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     @livewireStyles
     
 </head>
 
-<body class="bg-emerald-500 w-fit h-fit flex justify-center font-sans">
+<body class="w-fit h-fit flex justify-center font-sans">
     <div class="container container-fluid">
         <div class="flex justify-center min-h-screen">
             <main class="flex items-center">
