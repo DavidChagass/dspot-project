cc37b560bdeadbda9fe27ba5e043cc1a1d6a7005
Caik Lima <124724369+Caik0@users.noreply.github.com>
2024-10-14 21:18:09 -0300

    empresa btn password

--- resources/views/livewire/pages/auth/empresa-login.blade.php
+++ resources/views/livewire/pages/auth/empresa-login.blade.php
@@ -21,10 +21,16 @@
                     </div>
                 </div>
 
-                <div class="sm:col-span-4">
+                <div class="relative sm:col-span-4">
                     <label for="senha" class="block text-sm font-medium leading-6 text-gray-900">Senha</label>
                     <div class="mt-2">
                         <input id="password" type="password" wire:model="password" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+                    </div>
+                    <div class="toggle-button">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="eye-icon">
+                            <path d="M12 15a3 3 0 100-6 3 3 0 000 6z" />
+                            <path fill-rule="evenodd" d="M1.323 11.447C2.811 6.976 7.028 3.75 12.001 3.75c4.97 0 9.185 3.223 10.675 7.69.12.362.12.752 0 1.113-1.487 4.471-5.705 7.697-10.677 7.697-4.97 0-9.186-3.223-10.675-7.69a1.762 1.762 0 010-1.113zM17.25 12a5.25 5.25 0 11-10.5 0 5.25 5.25 0 0110.5 0z" clip-rule="evenodd" />
+                        </svg>
                     </div>
                 </div>
             </div>
