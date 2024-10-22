40a17ae485e5fec0b4264992194d7c8955e743b2
Caik0 <caikgiordane@gmail.com>
2024-10-22 15:15:36 -0300

    input email

--- resources/views/livewire/pages/auth/login.blade.php
+++ resources/views/livewire/pages/auth/login.blade.php
@@ -11,11 +11,16 @@
                 @enderror
             </div>
         </div>
+        <div class="sm:col-span-2 sm:col-start-1 pb-3">
+            <label for="email" class="block text-sm font-medium leading-6 text-gray-900">Email</label>
+            <div class="mt-2">
+                <input type="email" wire:model="email" id="email" name="email" class="bg-blue-100 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+                @error('email')
+                <span class="error">{{ $message }}</span>
+                @enderror
+            </div>
+        </div>
 
-        <input type="email" wire:model="email" placeholder="Email">
-        @error('email')
-        <span class="error">{{ $message }}</span>
-        @enderror
 
         <input type="password" wire:model="password" placeholder="Senha">
         @error('password')
