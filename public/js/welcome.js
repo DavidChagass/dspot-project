0de3e776d515bd0f6e9cde9b78c41640007408d4
Caik0 <caikgiordane@gmail.com>
2024-10-24 14:38:42 -0300

    Dropdown update

--- public/js/welcome.js
+++ public/js/welcome.js
@@ -1,6 +1,14 @@
 // Dropdown
-
-document.getElementById("bnt-dropdown").addEventListener("click", function() {
+document.getElementById("bnt-dropdown").addEventListener("click", function(event) {
     var dropdown = document.getElementById("dropdown");
     dropdown.classList.toggle("hidden");
+    event.stopPropagation(); // Impede que o clique no botão feche a dropdown imediatamente
+});
+
+// Fecha a dropdown se o usuário clicar fora dela
+document.addEventListener("click", function(event) {
+    var dropdown = document.getElementById("dropdown");
+    if (!dropdown.classList.contains("hidden") && !event.target.closest("#bnt-dropdown")) {
+        dropdown.classList.add("hidden");
+    }
 });
