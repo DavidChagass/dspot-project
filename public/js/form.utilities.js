7dbcb65088d6df638975cc5f2d6153b2ff4c3fec
Caik0 <caikgiordane@gmail.com>
2024-11-11 14:36:20 -0300

    btn update

--- public/js/form.utilities.js
+++ public/js/form.utilities.js
@@ -1,3 +1,13 @@
-document.getElementById("password").addEventListener("click", function(event) {
-    
+document.getElementsByClassName("toggle-button")[0].addEventListener("click", function () {
+    const svgAtiv = document.getElementById("passDesativo");
+    const svgDesativ = document.getElementById("passAtivo");
+
+    if (svgDesativ.classList.contains('hidden')) {
+        svgDesativ.classList.remove('hidden');
+        svgAtiv.classList.add("hidden");
+    } else {
+        svgAtiv.classList.remove('hidden');
+        svgDesativ.classList.add("hidden");
+    }
+
 });
