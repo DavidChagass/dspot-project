f464e9aa5633a189edaa1f4a097d4bad5c8caa9e
Caik0 <caikgiordane@gmail.com>
2024-11-21 20:25:06 -0300

    btn review password completo

--- public/js/form.utilities.js
+++ public/js/form.utilities.js
@@ -1,16 +1,29 @@
-document.getElementsByClassName("toggle-button")[0].addEventListener("click", function () {
+document.addEventListener("DOMContentLoaded", function (event) {
+    const toggleButton = document.getElementsByClassName("toggle-button")[0];
+    const inpPassword = document.getElementsByClassName("inpPassword")[0];
     const svgAtiv = document.getElementById("passDesativo");
     const svgDesativ = document.getElementById("passAtivo");
-    
 
-    if (svgDesativ.classList.contains('hidden')) {
-        svgDesativ.classList.remove('hidden');
-        svgAtiv.classList.add("hidden");
-    } else {
-        svgAtiv.classList.remove('hidden');
-        svgDesativ.classList.add("hidden");
+    if (!toggleButton || !inpPassword || !svgAtiv || !svgDesativ) {
+        console.error("Elementos necessários não encontrados!");
+        return;
     }
 
+    inpPassword.setAttribute("type", "password");
+
+    toggleButton.addEventListener("click", function (e) {
+        e.preventDefault(); // Impede o comportamento padrão (envio de formulário)
+
+        if (svgDesativ.classList.contains("hidden")) {
+            inpPassword.setAttribute("type", "text");
+            svgDesativ.classList.remove("hidden");
+            svgAtiv.classList.add("hidden");
+        } else {
+            inpPassword.setAttribute("type", "password");
+            svgAtiv.classList.remove("hidden");
+            svgDesativ.classList.add("hidden");
+        }
+    });
 });
 
 document.getElementsByClassName("voltarBtn")[0].addEventListener("click", function() {
