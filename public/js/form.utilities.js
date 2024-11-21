1813aaa98de71c649766de4b61ac0bc3c1a5dc0a
Caik0 <caikgiordane@gmail.com>
2024-11-21 19:07:39 -0300

    btnVoltar update

--- public/js/form.utilities.js
+++ public/js/form.utilities.js
@@ -1,17 +1,3 @@
-document.getElementsByClassName("toggle-button")[0].addEventListener("click", function () {
-    const svgAtiv = document.getElementById("passDesativo");
-    const svgDesativ = document.getElementById("passAtivo");
-
-    if (svgDesativ.classList.contains('hidden')) {
-        svgDesativ.classList.remove('hidden');
-        svgAtiv.classList.add("hidden");
-    } else {
-        svgAtiv.classList.remove('hidden');
-        svgDesativ.classList.add("hidden");
-    }
-
-});
-
-document.getElementById("voltarBtn").addEventListener("click", function() {
+document.getElementsByClassName("voltarBtn")[0].addEventListener("click", function() {
     history.back();
 });
--- resources/views/layouts/auth-layout.blade.php
+++ resources/views/layouts/auth-layout.blade.php
@@ -29,7 +29,7 @@
                     </div>
                     <div class="sm:ml-6 sm:block">
                         <div class="flex space-x-4">
-                            <button id="voltarBtn" class="rounded-md bg-gray-900 px-3 py-2 text-sm font-medium text-white" aria-current="page">Voltar</button>
+                            <button class="voltarBtn rounded-md bg-gray-900 px-3 py-2 text-sm font-medium text-white" aria-current="page">Voltar</button>
                         </div>
                     </div>
                 </div>
