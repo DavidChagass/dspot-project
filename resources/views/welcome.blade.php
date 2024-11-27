eaa2bcfcb3e91357ae2c5d2243d92b26c0edba70
Caik0 <caikgiordane@gmail.com>
2024-11-27 07:40:15 -0300

    Welcome update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,6 +12,7 @@
     <script defer src="{{ asset('js/welcome.js') }}"></script>
 
     <!-- Styles -->
+    <link rel="stylesheet" href="{{ asset('css/welcome.css') }}">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     <script src="https://cdn.tailwindcss.com"></script>
@@ -65,7 +66,12 @@
             </div>
         </div>
     </nav>
-
+    <section class="hero">
+        <div class="content-section">
+            <img src="{{ asset('/src/Group 3.png' )}}" alt="Logo" class="logo-section">
+            <h1 class="slogan-section">The best spot for your estoque</h1>
+        </div>
+    </section>
 </body>
 
 </html>
