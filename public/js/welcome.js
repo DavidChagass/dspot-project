d6cfdd580a3cd79c795529ff9ab3a95e88a1bc09
Caik0 <caikgiordane@gmail.com>
2024-11-27 23:25:14 -0300

    Starting landing page

--- public/js/welcome.js
+++ public/js/welcome.js
@@ -1,3 +1,9 @@
+const baseText = "O melhor lugar para o seu estoque.";
+const wordsToChange = ["o seu estoque", "a sua empresa", "o seu sucesso", "os seus resultados"];
+const typewriterElement = document.getElementById("typewriter");
+let index = 0;
+let wordIndex = 0; 
+
 // Dropdown
 document.getElementById("btn-dropdown").addEventListener("click", function(event) {
     var dropdown = document.getElementById("dropdown");
@@ -12,3 +18,52 @@ document.addEventListener("click", function(event) {
         dropdown.classList.add("hidden");
     }
 });
+
+document.getElementById('contactForm').addEventListener('submit', function (event) {
+
+    event.preventDefault();
+    alert('Mensagem enviada com sucesso!');
+    this.reset();
+});
+
+document.getElementById('contactForm').addEventListener('submit', function (event) {
+
+    event.preventDefault();
+    const name = this.querySelector('input[type="text"]').value;
+    const email = this.querySelector('input[type="email"]').value;
+    const message = this.querySelector('textarea').value;
+
+    if (name && email && message) {
+        alert('Mensagem enviada com sucesso!');
+        this.reset();
+    } else {
+        alert('Por favor, preencha todos os campos.');
+    }
+});
+
+function type() {
+
+    // Substitui a palavra na frase com base no índice atual
+    const modifiedText = baseText.replace(/o seu estoque/, wordsToChange[wordIndex]);
+    // Limpa o texto atual
+    typewriterElement.textContent = '';
+    index = 0;
+    // Função para digitar o texto modificado
+
+    function typeModifiedText() {
+
+        if (index < modifiedText.length) {
+            typewriterElement.textContent += modifiedText.charAt(index);
+            index++;
+            setTimeout(typeModifiedText, 100); // Tempo entre cada letra (100ms)
+        } else {
+            // Aguarda 2 segundos e inicia novamente
+            setTimeout(() => {
+                wordIndex = (wordIndex + 1) % wordsToChange.length; // Aumenta o índice da palavra
+                type(); // Inicia novamente com a nova palavra
+            }, 2000);
+        }
+    }
+    typeModifiedText();
+}
+type();
